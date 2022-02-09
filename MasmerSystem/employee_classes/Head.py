be495255155a3c064e55b9f0ba738f30f5518aa1
eilayll <98478880+eilayll@users.noreply.github.com>
2022-02-09 19:14:03 +0200

    חדש עילי

--- MasmerSystem/employee_classes/Head.py
+++ MasmerSystem/employee_classes/Head.py
@@ -1,1 +1,6 @@
 from employee_classes.Manager import *
+import Manager *
+
+class Head(Manager):
+    def __init__(self, first_mam, 43last_name, id_number, employee_id, role):
+        super().__init__(first_mam, last_name, id_number, employee_id, role)
