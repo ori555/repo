a8ad180bb3caa40d8b17324dee4efb88e196e44a
eilayll <98478880+eilayll@users.noreply.github.com>
2022-02-09 16:12:43 +0200

    חדש עילי

--- MasmerSystem/employee_classes/Employee.py
+++ MasmerSystem/employee_classes/Employee.py
@@ -1,3 +1,29 @@
 import Database
 from constants import *
 
+class EMPLOYEE():
+    def _init_(self, first_name, last_name, id_number, employee_id, manager_id, Role):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__id_number = id_number
+        self._employee_id = employee_id
+        self._manager_id = manager_id
+        self._Role = Role
+
+    def get_first_name(self):
+        return self.__first_name
+
+    def get_last_name(self):
+        return self.__last_name
+
+    def get_id_number(self):
+        return self.__id_number
+
+    def get_employee_id(self):
+        return self._employee_id
+
+    def get_manager_id(self):
+        return self._manager_id
+
+    def get_role(self):
+        return self._Role
