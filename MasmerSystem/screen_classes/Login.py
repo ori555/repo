169cdf8313d4b2d4099c7199c143ae4609ab5e76
אורי בן לולו <ori76563@gmail.com>
2022-02-15 14:46:33 +0200

    המשך מקרי קצה

--- MasmerSystem/screen_classes/Login.py
+++ MasmerSystem/screen_classes/Login.py
@@ -20,13 +20,28 @@ def check_login_details(username, password):
         return "images/errors/wrong_username_or_password.jpg"
             #WRONG_USERNAME_OR_PASSWORD_ERROR
 
+    username_numbers_sum = 0
+    for char in username:
+        if char.isnumeric():
+            username_numbers_sum += int(char)
+
     if username_numbers_sum % 10 != 0:
         return "images/errors/wrong_username_or_password.jpg"
             #WRONG_USERNAME_OR_PASSWORD_ERROR
 
+    password_numbers_sum = 0
+    for char in password:
+        password_numbers_sum += int(char)
+
     if password_numbers_sum != username_numbers_sum:
         return "images/errors/wrong_username_or_password.jpg"
             #WRONG_USERNAME_OR_PASSWORD_ERROR
+    if employee[5] == "e":
+        return Employee(employee[0], employee[1], employee[2], employee[3], employee[4], employee[5])
+    if employee[5] == "h":
+        return Head(employee[0], employee[1], employee[2], employee[3], employee[4], employee[5])
+    if employee[5] == "m":
+        return Manager(employee[0], employee[1], employee[2], employee[3], employee[4], employee[5])
 
 
 
